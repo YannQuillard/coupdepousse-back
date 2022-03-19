a4d495aff836473ee006c175b71a1204634398e0
Chriiiiiss <chris.fraanc@gmail.com>
2022-03-19 14:48:11 +0100

    🔧 Config Twilio Env && ADD Package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { DatabaseModule } from './database/database.module';
+import { TwilioModule } from './twilio_sms/twilio_sms.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    DatabaseModule
+    DatabaseModule,
+    TwilioModule
   ],
   controllers: [AppController],
   providers: [AppService],
