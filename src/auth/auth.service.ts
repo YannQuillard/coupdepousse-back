a09dc8670fc13113bbdfd0e07cdc6cf0f1453ea8
Yann Quillard <y38.quillard@gmail.com>
2022-03-25 00:03:09 +0100

    🔧 Destroy & existingphone

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, Inject, HttpException, HttpStatus, Logger } from '@nestjs/common';
 import { CreateTokenDto } from './dto/create-token.dto';
 import { CreateVerificationCodeDto } from './dto/create-verificationCode.dto';
 import { Token } from './token.model';
@@ -19,9 +19,16 @@ export class AuthService {
     }
 
     async createCode(createVerificationCodeDto: CreateVerificationCodeDto ): Promise<VerificationCode> {
-        const code = Math.floor(1000 + Math.random() * 9000);
-        createVerificationCodeDto.code = code;
-        return this.verificationCode.create(createVerificationCodeDto);
+        const existingPhone = await this.findPhone(createVerificationCodeDto.phone);
+        Logger.log(existingPhone);
+        if(existingPhone === null) {
+            const code = Math.floor(1000 + Math.random() * 9000);
+            createVerificationCodeDto.code = code;
+            return this.verificationCode.create(createVerificationCodeDto);
+        }
+        else {
+
+        }
     }
 
     async checkCode(code: number, phone: string) {
@@ -35,6 +42,21 @@ export class AuthService {
         if(!result) {
             throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
         }
-        return result
+        return result;
+    }
+
+    async findPhone(phone: string) {
+        const result = await this.verificationCode.findOne({
+            where: {
+                phone
+            }
+        });
+
+        return result;
+    }
+
+    async deleteCode(phone: string) {
+        const code = await this.findPhone(phone);
+        await code.destroy()
     }
 }
