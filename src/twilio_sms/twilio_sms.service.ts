60b3f78111a005134d17cb03f93d3fd3855cf238
Chriiiiiss <chris.fraan@gmail.com>
2022-03-24 16:34:50 +0100

    📚 Database SignupMobile

--- src/twilio_sms/twilio_sms.service.ts
+++ src/twilio_sms/twilio_sms.service.ts
@@ -1,6 +1,11 @@
 import { Inject, Injectable, Logger } from "@nestjs/common";
 import { sendSms } from "./func/send_sms";
+import {User} from '../users/user.model'
 import { UsersService } from "src/users/users.service";
+import { twiml } from "twilio";
+import { create } from "domain";
+
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 @Injectable()
 export class TwilioServices {
@@ -12,16 +17,5 @@ export class TwilioServices {
 
     sendSms(phoneNbr: string): string {
         return sendSms(phoneNbr);
-    }
-
-    checkNbr(phoneNbr: string) {
-        if(this.userServices.findOneByPhone("8336702") !== undefined) {
-            Logger.log(`J'ai retrouver le num : ${phoneNbr} dans la base de données`);
-            return true
-        } else {
-            Logger.log(`Je n'ai pas retrouver le num : ${phoneNbr} dans la base de données`);
-            return false
-        }
-
     }
 }
